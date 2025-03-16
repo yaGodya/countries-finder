ac851049b9c862c6a650d51f009d607e5fc21c32
yaGodya <krispygeorg@gmail.com>
2025-03-16 22:19:50 +0200

    implement theme change, add theme reducer and theme action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,68 @@
-import styled from 'styled-components';
-import {useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { IoMoon, IoMoonOutline } from 'react-icons/io5';
+import styled from "styled-components";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
 
-import { Container } from './Container';
+import { Container } from "./Container";
+import { useDispatch, useSelector } from "react-redux";
+import { setTheme } from "../store/theme/theme-actions";
 
 const HeaderEl = styled.header`
-  box-shadow: var(--shadow);
-  background-color: var(--colors-ui-base);
+    box-shadow: var(--shadow);
+    background-color: var(--colors-ui-base);
 `;
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 2rem 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 2rem 0;
 `;
 
 const Title = styled(Link).attrs({
-  to: '/',
+    to: "/",
 })`
-  color: var(--colors-text);
-  font-size: var(--fs-sm);
-  text-decoration: none;
-  font-weight: var(--fw-bold);
+    color: var(--colors-text);
+    font-size: var(--fs-sm);
+    text-decoration: none;
+    font-weight: var(--fw-bold);
 `;
 
 const ModeSwitcher = styled.div`
-  color: var(--colors-text);
-  font-size: var(--fs-sm);
-  cursor: pointer;
-  // font-weight: var(--fw-bold);
-  text-transform: capitalize;
+    color: var(--colors-text);
+    font-size: var(--fs-sm);
+    cursor: pointer;
+    text-transform: capitalize;
 `;
 
 export const Header = () => {
-  const theme = 'light';
+    const dispatch = useDispatch();
+    const theme = useSelector((state) => state.theme);
 
-  useEffect(() => {
-    document.body.setAttribute('data-theme', theme);
-  }, [theme]);
+    const toggleTheme = () =>
+        dispatch(setTheme(theme === "light" ? "dark" : "light"));
 
-  return (
-    <HeaderEl>
-      <Container>
-        <Wrapper>
-          <Title>Where is the world?</Title>
-          <ModeSwitcher>
-            {theme === 'light' ? (
-              <IoMoonOutline size="14px" />
-            ) : (
-              <IoMoon size="14px" />
-            )}{' '}
-            <span style={{ marginLeft: '0.75rem' }}>{theme} Theme</span>
-          </ModeSwitcher>
-        </Wrapper>
-      </Container>
-    </HeaderEl>
-  );
+    useEffect(() => {
+        document.body.setAttribute("data-theme", theme);
+    }, [theme]);
+
+    return (
+        <HeaderEl>
+            <Container>
+                <Wrapper>
+                    <Title>Where is the world?</Title>
+                    <ModeSwitcher onClick={toggleTheme}>
+                        {theme === "light" ? (
+                            <IoMoonOutline size="14px" />
+                        ) : (
+                            <IoMoon size="14px" />
+                        )}{" "}
+                        <span style={{ marginLeft: "0.75rem" }}>
+                            {theme} Theme
+                        </span>
+                    </ModeSwitcher>
+                </Wrapper>
+            </Container>
+        </HeaderEl>
+    );
 };
---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import { compose, applyMiddleware, createStore } from "redux";
+import { rootReducer } from "./root-reducer";
+import thunk from "redux-thunk";
+import axios from "axios";
+import * as api from '../config'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk.withExtraArgument({ client: axios, api }))
+    )
+);
+
+export { store };
